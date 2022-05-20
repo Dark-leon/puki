15ba4d6da3b3cf05b187aecd5561009d14f9e05b
Pulsar554706 <100949028+Pulsar8806@users.noreply.github.com>
2022-05-20 12:46:35 +0300

    Update playmode.py

--- YukkiMusic/plugins/play/playmode.py
+++ YukkiMusic/plugins/play/playmode.py
@@ -1,12 +1,3 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
 
@@ -32,19 +23,19 @@ PLAYMODE_COMMAND = get_command("PLAYMODE_COMMAND")
 async def playmode_(client, message: Message, _):
     playmode = await get_playmode(message.chat.id)
     if playmode == "Direct":
-        Direct = True
+        Direct = false
     else:
         Direct = None
     is_non_admin = await is_nonadmin_chat(message.chat.id)
     if not is_non_admin:
-        Group = True
+        Group = false
     else:
         Group = None
     playty = await get_playtype(message.chat.id)
     if playty == "Everyone":
         Playtype = None
     else:
-        Playtype = True
+        Playtype = false
     buttons = playmode_users_markup(_, Direct, Group, Playtype)
     response = await message.reply_text(
         _["playmode_1"].format(message.chat.title),
