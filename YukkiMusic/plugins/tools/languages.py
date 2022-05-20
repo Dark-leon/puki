fc9a3f6a8ff3e593947fa924108b1ddfed0ea847
Pulsar554706 <100949028+Pulsar8806@users.noreply.github.com>
2022-05-20 12:49:04 +0300

    Update languages.py

--- YukkiMusic/plugins/tools/languages.py
+++ YukkiMusic/plugins/tools/languages.py
@@ -1,12 +1,3 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
 from pykeyboard import InlineKeyboard
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, Message
@@ -49,7 +40,7 @@ def lanuages_keyboard(_):
             callback_data=f"languages:gu",
         ),
         InlineKeyboardButton(
-            text="🇹🇷 Türkiye Türkçesi",
+            text="🇹🇷 Türkiye",
             callback_data=f"languages:tr",
         ),
     )
